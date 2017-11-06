885b707e7207ec79a09b759540f29f18400dfadc
silfrasharath <33340413+silfrasharath@users.noreply.github.com>
2017-11-06 11:28:33 +0530

    Update MainActivity.java

--- app/src/main/java/com/svb1/dell/silfralogs/MainActivity.java
+++ app/src/main/java/com/svb1/dell/silfralogs/MainActivity.java
@@ -7,9 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.svb1.dell.silfralogging.LogDebug;
-import com.svb1.dell.silfralogging.LogError;
-import com.svb1.dell.silfralogging.LogInfo;
+import static com.svb1.dell.silfralogging.LogDebug.logDebug;
+import static com.svb1.dell.silfralogging.LogError.logError;
+import static com.svb1.dell.silfralogging.LogInfo.logInfo;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -54,43 +54,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         switch (id){
             case R.id.debug_text:
-                LogDebug.silfraLogs("Silfra Debug");
+                logDebug("Silfra Debug");
                 break;
 
             case R.id.debug_variable:
                 String debug=StringD.getText().toString();
-                LogDebug.silfraLogs(debug);
+                logDebug(debug);
                 break;
 
             case R.id.debug_null:
-                LogDebug.silfraLogs(null);
+                logDebug(null);
                 break;
 
             case R.id.info_text:
-                LogInfo.silfraLogs("Silfra Info");
+                logInfo("Silfra Info");
                 break;
 
             case R.id.info_variable:
                 String info=StringI.getText().toString();
-                LogInfo.silfraLogs(info);
+                logInfo(info);
                 break;
 
             case R.id.info_null:
-                LogInfo.silfraLogs(null);
+                logInfo(null);
                 break;
 
             case R.id.error_text:
-                LogError.silfraLogs("Silfra Error");
+                logError("Silfra Error");
                 break;
 
             case R.id.error_variable:
                 String error=StringE.getText().toString();
-                LogError.silfraLogs(error);
-
+                logError(error);
                 break;
 
             case R.id.error_null:
-                LogError.silfraLogs(null);
+                logError(null);
                 break;
         }
     }
